b964b29d09d8b0e6487469f9f2109f66296f9f70
andrew <andrew@buildtheproduct.com>
2016-12-15 14:38:12 -0800

    added name

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello world"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
 
 if __name__ == "__main__":
     app.run()
